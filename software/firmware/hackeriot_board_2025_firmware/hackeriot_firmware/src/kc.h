31c4738eab26edbac6951321748891f42564d663
Benny Meisels <benny.meisels@gmail.com>
2025-08-24 21:15:36 +0300

    Add include guard to kc.h

--- software/firmware/hackeriot_board_2025_firmware/hackeriot_firmware/src/kc.h
+++ software/firmware/hackeriot_board_2025_firmware/hackeriot_firmware/src/kc.h
@@ -1,6 +1,11 @@
+#ifndef __KC_H__
+#define __KC_H__
+
 struct konami_code_t {
-	unsigned state	: 4;	// number of matches so far
-	unsigned active	: 1;
+	unsigned int state	: 4;	// number of matches so far
+	unsigned int active	: 1;
 };
 
 extern struct konami_code_t konami_code;
+
+#endif // __KC_H__
