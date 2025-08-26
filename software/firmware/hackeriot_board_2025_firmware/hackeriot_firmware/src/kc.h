a5fbdd10bb3b63b2d1c70978676c8574cd87a78b
Rani Hod <rani.hod@gmail.com>
2025-08-26 20:39:10 +0300

    Move LED breath stuff from main.c to kc.c

--- software/firmware/hackeriot_board_2025_firmware/hackeriot_firmware/src/kc.h
+++ software/firmware/hackeriot_board_2025_firmware/hackeriot_firmware/src/kc.h
@@ -1,11 +1,18 @@
+/*
+ * Copyright (c) 2025 Benny Meisels <benny.meisels@gmail.com>
+ *                    Rani Hod <rani.hod@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #ifndef __KC_H__
 #define __KC_H__
+
+
 
 struct konami_code_t {
 	unsigned int state	: 4;	// number of matches so far
 	unsigned int active	: 1;
 };
-
-extern struct konami_code_t konami_code;
 
 #endif // __KC_H__
