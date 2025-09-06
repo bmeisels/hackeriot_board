d8f62c91e5420fef6c8819f3a68029e814d0d108
Rani Hod <rani.hod@gmail.com>
2025-09-06 18:27:50 +0300

    Implemented Simon game
    Some unexplained lock-ups occur upon playr input.
    Also screen artifacts. Need to debug.

--- software/firmware/hackeriot_board_2025_firmware/hackeriot_firmware/src/simon.c
+++ software/firmware/hackeriot_board_2025_firmware/hackeriot_firmware/src/simon.c
@@ -10,9 +10,157 @@
 #include <zephyr/sys/printk.h>
 
 #include "buttons.h"
+#include "led.h"
 #include "simon.h"
+
+static char simon_dir(char dir)
+{
+    switch (dir) {
+        case 'U':
+        case 'D':
+        case 'L':
+        case 'R':
+            break;
+        default:
+            dir = "UDLR"[sys_rand8_get() & 3];            
+    }
+    //printf("[%s] dir=%c\n", __func__, dir);
+    return dir;
+}
+
+static uint64_t simon_glyph(char ch)
+{
+    switch(ch) {
+        case 'U':   return 0x183C7EFF18181818;
+        case 'D':   return 0x18181818FF7E3C18;
+        case 'L':   return 0x103070FFFF703010;
+        case 'R':   return 0x080CDEFFFF0E0C08;
+        case 'A':   return 0x3C7EE7C3FFFFC3C3;
+        case 'B':   return 0xFEC3C3FEFEC3C3FE;
+        default:    return 0;
+    };
+}
+
+static bool do_one_round(const struct device *led, struct simon_data_t *sd)
+{
+    static const char options[] = "UDLRAB";
+
+    // randomize sequence
+    for (unsigned i = 0; i < sd->len; i++)
+        sd->seq[i] = options[sys_rand32_get() % (ARRAY_SIZE(options)-1)];
+
+    // display sequence
+    uint64_t cur = 0, old;
+    printk("Simon:");
+    for (unsigned i = 0; i < sd->len; i++) {
+        char ch = sd->seq[i];
+        printk(" %c", ch);
+        old = cur;
+        cur = simon_glyph(ch);
+        led_swipe(led, old, cur, simon_dir(ch), 50);
+        k_msleep(SIMON_DELAY);
+    }
+    printk("\n");
+
+#if 0
+    buttons_clear();
+#endif
+
+    old = cur;
+    cur = led_glyph('?');
+    led_swipe(led, old, cur, 'L', 50);
+
+    bool game_on = true;
+    // query sequence
+    printk("Player:");
+    for (unsigned i = 0; i < sd->len && game_on; i++) {
+        char ch = buttons_get(options, K_MSEC(2 * SIMON_DELAY));
+        if (ch) printk(" %c", ch); else printk(" (none)");
+        old = cur;
+        cur = simon_glyph(ch);
+        if (ch != sd->seq[i]) {
+            game_on = false;
+            cur = ~cur;
+            printk(" <-- error");
+        }
+        led_swipe(led, old, cur, simon_dir(ch), 50);
+    }
+    printk("\n");
+
+    if (game_on) {
+        led_swipe(led, cur, SIMON_GLYPH_OK, 'L', 50);
+        printk("OK\n");
+    }
+
+    k_msleep(SIMON_DELAY * 3);
+
+    return game_on;
+}
+
+// returns score
+static unsigned do_one_game(const struct device *led)
+{
+    struct simon_data_t sd = {.points = 0, .len=INITIAL_SIMON_LEN};
+
+    uint64_t cur = 0, old;
+
+    // display Ready321
+	for (const char *c = "Ready?"; *c; ++c) {
+		old = cur;
+		cur = led_glyph(*c);
+		led_swipe(led, old, cur, 'L', 50);
+	}
+    for (unsigned i = 3; i; --i) {
+        k_msleep(SIMON_DELAY);
+		old = cur;
+		cur = led_glyph('0' + i);
+		led_swipe(led, old, cur, 'D', 50);
+    }
+	led_swipe(led, cur, 0, 'D', 50);
+    cur = 0;
+
+    while(do_one_round(led, &sd))
+    {
+        ++sd.len;
+        ++sd.points;
+
+    	led_swipe(led, SIMON_GLYPH_OK, 0, 'L', 50);
+    }
+
+    return sd.points;
+}
 
 void play_simon(const struct device *led)
 {
+    while (1) {
+        printk("[%s] new game\n", __func__);
+        unsigned points = do_one_game(led);
 
+        printk("[%s] game ended, score=%u\n", __func__, points);
+
+        // TODO: check for new highscore
+        
+        // display score
+        char score[10] = "Score: 00";
+        if (points < 10) {
+            score[7] += points % 10;
+            score[8] = '\0';
+        } else {
+            score[7] += points / 10;
+            score[9] += points % 10;
+        }
+        unsigned i = 0;
+        uint64_t old = 0;
+        while(1) {
+            char ch = score[i++];
+            if ( ! ch) { i = 0; ch = ' '; }
+            uint64_t cur = led_glyph(ch);
+            led_swipe(led, old, cur, 'L', 50);
+            old = cur;
+
+            ch = buttons_get("AB", K_NO_WAIT);
+            if (ch == 'A') break;   // another game
+            if (ch == 'B') return;  // to menu
+        }
+    }
 }
--- software/firmware/hackeriot_board_2025_firmware/hackeriot_firmware/src/simon.h
+++ software/firmware/hackeriot_board_2025_firmware/hackeriot_firmware/src/simon.h
@@ -8,10 +8,16 @@
 #ifndef __SIMON_H__
 #define __SIMON_H__
 
+#define INITIAL_SIMON_LEN 3
+#define SIMON_DELAY 700
+#define SIMON_MAX_LEN 30
 
+#define SIMON_GLYPH_OK 0x0065959696956500ULL
 
 struct simon_data_t {
-
+	uint8_t points;
+    uint8_t len;
+    char seq[SIMON_MAX_LEN];
 };
 
 void play_simon(const struct device *led);
